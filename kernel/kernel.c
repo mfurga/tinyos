bc919e9158927804c557d67f061baede929e4b47
Mateusz Furga <matfurga@gmail.com>
2023-03-10 21:27:29 +0100

    Update GDT

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -9,17 +9,6 @@
 #include <drivers/rtc.h>
 #include <drivers/keyboard.h>
 
-void print_memory_map(const boot_params_t *params) {
-  printf("BIOS-e820 physical RAM map:\n");
-
-  for (u16 i = 0; i < params->memory_entries; i++) {
-    printf("[%16lx - %16lx] %d\n",
-      params->memory_map[i].addr,
-      params->memory_map[i].addr + params->memory_map[i].length,
-      params->memory_map[i].type);
-  }
-}
-
 void CDECL NORETURN kernel_main(const boot_params_t *params) {
 
   video_init(params->video.mode,
@@ -34,8 +23,6 @@ void CDECL NORETURN kernel_main(const boot_params_t *params) {
 
   gdt_setup();
 
-  sti();
-
   pit_init(100);
 
   keyboard_init();
@@ -44,18 +31,6 @@ void CDECL NORETURN kernel_main(const boot_params_t *params) {
     printf("CPUID instruction is not supported!\n");
     for(;;);
   }
-
-  rtc_datetime_t datetime = rtc_read_datetime();
-
-  printf("%d %d %d %d:%d:%d\n",
-    datetime.day,
-    datetime.month,
-    datetime.year,
-    datetime.hour,
-    datetime.minute,
-    datetime.second);
-
-  print_memory_map(params);
 
   int3();
 
