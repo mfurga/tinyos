ec5472546ab8f10e47c9e4563580531f7a0b56ce
Mateusz Furga <matfurga@gmail.com>
2023-04-06 19:06:04 +0200

    Add print syscall

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -2,11 +2,32 @@
 #include <kernel/interrupt/idt.h>
 #include <kernel/stdio.h>
 
+typedef void (*syscall_handler_t)(int_context_t *);
+
+static void syscall_print(int_context_t *context);
+
+enum {
+  SYSCALL_PRINT = 0,
+  __SYSCALL_END
+};
+
+static syscall_handler_t syscalls[] = {
+  [SYSCALL_PRINT] = &syscall_print
+};
+
 static void syscall_handler(int_context_t *context) {
-  printf("SYSCALL!!!\n");
+  if (context->eax >= __SYSCALL_END) {
+    return;
+  }
+  syscalls[context->eax](context);
 }
 
 void syscall_init(void) {
   int_handler_register(SYSCALL_VECTOR, &syscall_handler);
+}
+
+static void syscall_print(int_context_t *context) {
+  printf((char *)context->ebx);
+  return;
 }
 
--- user/main.c
+++ user/main.c
@@ -1,9 +1,12 @@
 /* User mode program. */
 
 void user_main(void) {
+  const char *s = "Hello world!\n";
 
   __asm__ __volatile__(
+    "mov eax, 0;"
     "int 0x80;"
+    : : "b"(s)
   );
 
   for (;;);
